73fab3a93f13b92167dd48d5ab7f6d60b5632b5e
abdelhak <abdelhakmokri@gmail.com>
2023-04-08 00:22:32 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import './NavBar.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
+import React from 'react';
 
-const API_KEY = "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";
-// "Explain things like you would to a 10 year old learning how to code."
-const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
-  "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
+const API_KEY = "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";
+const systemMessage = {
+  "role": "system", "content": "Explain things like you're a product manager with experience as product owner and full stack developer talking to a software professional with 2 years of experience. include acceptance criteria"
 }
 
 function App() {
   const [messages, setMessages] = useState([
     {
-      message: "Hello, I'm ChatGPT! Ask me anything!",
+      message: "Hello, I'm TaskGPT! Lets create tasks for your project!",
       sentTime: "just now",
-      sender: "ChatGPT"
+      sender: "TaskGPT"
     }
   ]);
   const [isTyping, setIsTyping] = useState(false);
+  const [includeAcceptanceCriteria, setIncludeAcceptanceCriteria] = useState(false);
+  const [estimatedTime, setEstimatedTime] = useState(false);
+  const [stepsToBuild, setStepsToBuild] = useState(false);
+  const [teamMembers, setTeamMembers] = useState(1);
 
   const handleSend = async (message) => {
     const newMessage = {
@@ -27,20 +32,14 @@ function App() {
     };
 
     const newMessages = [...messages, newMessage];
-    
+
     setMessages(newMessages);
 
-    // Initial system message to determine ChatGPT functionality
-    // How it responds, how it talks, etc.
     setIsTyping(true);
     await processMessageToChatGPT(newMessages);
   };
 
-  async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
-    // Format messages for chatGPT API
-    // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
-    // So we need to reformat
-
+  async function processMessageToChatGPT(chatMessages) {
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
       if (messageObject.sender === "ChatGPT") {
@@ -48,61 +47,136 @@ function App() {
       } else {
         role = "user";
       }
-      return { role: role, content: messageObject.message}
+      let ac = includeAcceptanceCriteria ? 'include acceptance criteria' : ',';
+      let et = estimatedTime ? 'include etimated time' : ',';
+      let sb = stepsToBuild ? 'include steps to build':',';
+
+      let fullMessage = "give me full detailed Jira like tasks for this project details:" + messageObject.message  + ac + et + sb + 'team mebers ='+teamMembers
+
+      return { role: role, content: fullMessage}
     });
 
-
-    // Get the request body set up with the model we plan to use
-    // and the messages which we formatted above. We add a system message in the front to'
-    // determine how we want chatGPT to act. 
     const apiRequestBody = {
       "model": "gpt-3.5-turbo",
       "messages": [
-        systemMessage,  // The system message DEFINES the logic of our chatGPT
-        ...apiMessages // The messages from our chat with ChatGPT
+        systemMessage,
+        ...apiMessages
       ]
     }
 
-    await fetch("https://api.openai.com/v1/chat/completions", 
-    {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer " + API_KEY,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(apiRequestBody)
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
-      console.log(data);
-      setMessages([...chatMessages, {
-        message: data.choices[0].message.content,
-        sender: "ChatGPT"
-      }]);
-      setIsTyping(false);
-    });
+    await fetch("https://api.openai.com/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          "Authorization": "Bearer " + API_KEY,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(apiRequestBody)
+      }).then((data) => {
+        return data.json();
+      }).then((data) => {
+        console.log(data);
+        setMessages([...chatMessages, {
+          message: data.choices[0].message.content,
+          sender: "ChatGPT"
+        }]);
+        setIsTyping(false);
+      });
+  }
+
+  const handleCheckboxChange = (e) => {
+    const { name, checked } = e.target;
+    switch (name) {
+      case 'includeAcceptanceCriteria':
+        setIncludeAcceptanceCriteria(checked);
+        break;
+      case 'estimatedTime':
+        setEstimatedTime(checked);
+        break;
+      case 'stepsToBuild':
+        setStepsToBuild(checked);
+        break;
+      default:
+        break;
+    }
+  }
+
+  const handleTeamMembersChange = (e) => {
+    setTeamMembers(e.target.value);
   }
 
   return (
     <div className="App">
-      <div style={{ position:"relative", height: "800px", width: "700px"  }}>
-        <MainContainer>
+  <div style={{ position:"relative", height: "800px", width: "900px"  }}>
+
+      <nav>
+        <div className="logo">
+          <a href="/">TasksGPT</a>
+        </div>
+        
+        <div className="checkboxes">
+        <label htmlFor="includeAcceptanceCriteria">
+        <input
+          type="checkbox"
+          id="includeAcceptanceCriteria"
+          name="includeAcceptanceCriteria"
+          checked={includeAcceptanceCriteria}
+          onChange={handleCheckboxChange}
+        />
+        Include Acceptance Criteria
+      </label>
+      <label htmlFor="estimatedTime">
+        <input
+          type="checkbox"
+          id="estimatedTime"
+          name="estimatedTime"
+          checked={estimatedTime}
+          onChange={handleCheckboxChange}
+        />
+        Estimated Time
+      </label>
+      <label htmlFor="stepsToBuild">
+        <input
+          type="checkbox"
+          id="stepsToBuild"
+          name="stepsToBuild"
+          checked={stepsToBuild}
+          onChange={handleCheckboxChange}
+        />
+        Steps to Build
+      </label>
+    </div>
+    <div className="teamMembers">
+      <label htmlFor="teamMembers">
+        Team Members:
+        <input
+          type="number"
+          id="teamMembers"
+          name="teamMembers"
+          value={teamMembers}
+          onChange={handleTeamMembersChange}
+        />
+      </label>
+    </div>
+  </nav>
+
+  <MainContainer>
           <ChatContainer>       
             <MessageList 
               scrollBehavior="smooth" 
-              typingIndicator={isTyping ? <TypingIndicator content="ChatGPT is typing" /> : null}
+              typingIndicator={isTyping ? <TypingIndicator content="TaskGPT is typing" /> : null}
             >
               {messages.map((message, i) => {
                 console.log(message)
                 return <Message key={i} model={message} />
               })}
             </MessageList>
-            <MessageInput placeholder="Type message here" onSend={handleSend} />        
+            <MessageInput placeholder="Enter your project details here" onSend={handleSend} />        
           </ChatContainer>
         </MainContainer>
-      </div>
-    </div>
-  )
+        </div>
+</div>
+);
 }
 
-export default App
+export default App;
